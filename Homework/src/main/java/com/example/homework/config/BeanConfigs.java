844c9d9a41a62a405165754230743c76cb219b9a
buckaroo69 <diogobento@ua.pt>
2021-05-02 13:49:45 +0100

    fixes folder structure

--- Homework/src/main/java/com/example/homework/config/BeanConfigs.java
+++ Homework/src/main/java/com/example/homework/config/BeanConfigs.java
@@ -1,11 +1,13 @@
-package config;
+package com.example.homework.config;
 
 import com.example.homework.HomeworkApplication;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class BeanConfigs {
     @Bean
     Logger logger(){return  LoggerFactory.getLogger(HomeworkApplication.class);}
--- Homework/src/main/java/com/example/homework/controller/Controller.java
+++ Homework/src/main/java/com/example/homework/controller/Controller.java
@@ -1,4 +1,4 @@
-package controller;
+package com.example.homework.controller;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;

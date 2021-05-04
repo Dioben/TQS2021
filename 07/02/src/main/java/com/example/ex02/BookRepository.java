aea216a8d860daa21c3423ee87d3e14e04ea62ae
buckaroo69 <diogobento@ua.pt>
2021-05-04 14:37:53 +0100

    repo annotation, still broken

--- 07/02/src/main/java/com/example/ex02/BookRepository.java
+++ 07/02/src/main/java/com/example/ex02/BookRepository.java
@@ -2,6 +2,8 @@ package com.example.ex02;
 
 import com.example.ex02.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
